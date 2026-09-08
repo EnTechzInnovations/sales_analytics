b29e56a8665e1fb04d504ba13e9810bebdd6487e
EN-Techz <entechzinnovations@gmail.com>
2026-09-08 10:56:36 +0530

    six commit

--- owner_app/urls.py
+++ owner_app/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('owner_home',views.owner_home,name='owner_home'),
     path('owner_manage_branch',views.owner_manage_branch,name='owner_manage_branch'),
     path('owner_delete_branch/<id>',views.owner_delete_branch,name='owner_delete_branch'),
+    path('owner_update_branch/<id>',views.owner_update_branch,name='owner_update_branch'),
+    path('owner_manage_staff',views.owner_manage_staff,name='owner_manage_staff'),
+    path('owner_delete_staff/<id>',views.owner_delete_staff,name='owner_delete_staff'),
+    
+
  
     
    
--- owner_app/views.py
+++ owner_app/views.py
@@ -1,3 +1,4 @@
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -15,7 +16,7 @@ def owner_home(request):
 
 
 def owner_manage_branch(request):
-    data=branch.objects.filter(owner_id=request.session['owner_id'])
+    x=branch.objects.filter(owner_id=request.session['owner_id'])
 
     if request.method=='POST':
         branch_name=request.POST['branch_name']
@@ -28,11 +29,27 @@ def owner_manage_branch(request):
 
         return HttpResponse("<script>alert('Branch Added Successfully');window.location='/owner_manage_branch';</script>")
 
-    return render(request,'owner_manage_branch.html',{'data':data})
+    return render(request,'owner_manage_branch.html',{'x':x})
 
 
 def owner_update_branch(request,id):
-    return render(request,'owner_manage_branch.html')
+    data=branch.objects.get(branch_id=id)
+
+    if request.method=='POST':
+        brach_name=request.POST['branch_name']
+        location=request.POST['location']
+        phone=request.POST['phone']
+        manager_name=request.POST['manager_name']
+
+        data.branch_name=brach_name
+        data.location=location
+        data.phone=phone
+        data.manager_name=manager_name
+        data.save()
+        return HttpResponse("<script>alert('Branch Updated Successfuly');window.location='/owner_manage_branch';</script>")
+
+    return render(request,'owner_manage_branch.html',{'data':data})
+
 
 def owner_delete_branch(request,id):
     x=branch.objects.get(branch_id=id)
@@ -40,8 +57,46 @@ def owner_delete_branch(request,id):
     return HttpResponse("<script>alert('Delete Successfully');window.location='/owner_manage_branch';</script>")
 
 
-def manage_staff(request):
+def owner_manage_staff(request):
+    br=branch.objects.filter(owner_id=request.session['owner_id'])
+    data=staff.objects.filter(branch__owner_id=request.session['owner_id'])
 
-    return render(request,'owner_manage_staff.html')
+    if request.method=='POST':
+        branch_id=request.POST['branch_id']
+        staff_name=request.POST['staff_name']
+        gender=request.POST['gender']
+        age=request.POST['age']
+        phone=request.POST['phone']
+        email=request.POST['email']
+        address=request.POST['address']
+        joining_date=request.POST['joining_date']
+        photo=request.FILES['photo']
+        username=request.POST['uname']
+        password=request.POST['password']
 
 
+        fs= FileSystemStorage()
+        image=fs.save(photo.name,photo)
+
+        log=login(user_name=username,password=password,user_type='staff')
+        log.save()
+
+        add=staff(branch_id=branch_id,name=staff_name,gender=gender,age=age,phone=phone,email=email,address=address,joining_date=joining_date,photo=image,login_id=log.pk)
+        add.save()
+
+        return HttpResponse("<script>alert('Staff Added Successfully');window.location='/owner_manage_staff';</script>")
+
+    return render(request,'owner_manage_staff.html',{'br':br,'data':data})
+
+
+
+def owner_delete_staff(request,id):
+    x=staff.objects.get(staff_id=id)
+    x.delete()
+    return HttpResponse("<script>alert('Delete Successfully');window.location='/owner_manage_staff';</script>")
+
+
+
+def owner_update_staff(request,id):
+        
+    return  render(request,'owner_manage_staff.html')
